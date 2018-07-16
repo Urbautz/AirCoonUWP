d395c23ca9a1c8d8f6a4953f5321f04bf7781813
Urbautz <40495078+Urbautz@users.noreply.github.com>
2018-07-16 11:41:02 +0200

    Basic Manufacturer Data & Constructor

---
+++ AirCoonUWP/Game/Models/Aircraft/Manufacturer.cs
@@ -0,0 +1,39 @@
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Queue;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCoon.Game.Handler;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AirCoon.Game.Models.Aircraft
+{
+    [Serializable()]
+    public class Manufacturer
+        : ISerializable
+    {
+    
+        readonly String Name;
+        private ProductionLine[] ProductionLines;
+        private Queue OrderBook;
+        private List<Aircraft> _AircraftInStock;
+        public List<Aircraft> AircraftInStock {
+            get { return _AircraftInStock;}
+        }
+            
+        
+        public Manufacturer(String name, int productionlines, int capacity) {
+           this.Name = name;
+           ProductionLines[] = new ProductionLine[productionlines]
+           for (int i=0;i<productionlines;i++) {
+             ProductionLines[i] = new ProductionLine(this, capacity);
+           }
+           this.OrderBook = new Queue();
+           this._AircraftInStock = new List<Aircraft>;
+        }
+    
+    } // End Class
+} // End Namespace
