375c94a4c28e26d5a543745e5167996475133ac1
Urbautz <40495078+Urbautz@users.noreply.github.com>
2018-07-18 11:35:45 +0200

    Update ProductionLine.cs

--- AirCoonUWP/Game/Models/Aircraft/ProductionLine.cs
+++ AirCoonUWP/Game/Models/Aircraft/ProductionLine.cs
@@ -14,38 +14,49 @@ namespace AirCoon.Game.Models.Aircraft
     public class ProductionLine
         : ISerializable, IAmTickable
     {
+    
+        readonly Manufacturer Manufacturer;
         readonly int Capacity;
         private int _Progress;
         public int Progress {
             get { return _Progress;}
         }
-        private Plane AircaftWIP = null;
-
-        private Plane _AircraftNext = null;
-        public Aircraft AircraftNext {
-          get { return _AircraftNext; }
-        }
+        private Plane PlaneWIP = null;
             
         
-        public ProductionLine(int capacity) {
+        public ProductionLine(Manufacturer manufacturer, int capacity) {
            this.Capacity = capacity;
+           this.Manufacturer = manufacturer;
         } // end Constructor
         
         public ProductionLIne(SerializationInfo info, StreamingContext ctxt) {
-          this.Name = info.GetString("Name');
-          blablah missing
-        } // End Deserializer
+          m = info.GetString("Manufacturer");  
+          this.Manufacturer = SaveGamePublic.SaveGame.Manufacturers[m];
+          this.Capacity = info.GetInt32("Capacity");
+          this._Progress = info.GetInt32("Progress");
+          try 
+          {
+            this._PlaneWIP = (Plane)info.GetValue("PlaneWIP", typeof(Plane));
+          } catch (Exception e) {
+            this._PlaneWIP = null;
+          }
+        }// End Deserializer
     
-        public bool SetNextAircraft(Aircraft ac) {
-             if(this.AircraftWIP == null) {
-                  this.AircraftWIP = ac;
+        // Serialiszer
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("Manufacturer", this.Manufacturer.Name);
+            info.AddValue("Capacity", this.Capacity);
+            info.AddValue("Progress", this.Progress);
+            info.AddValue("PlaneWIP", this.PlaneWIP);
+        } // end GetObjectData
+
+        public bool StartPlaneBuild(Plane plane) {
+             if(this.PlaneWIP == null) {
+                  this.PlaneWIP = ac;
                   return true;
              } 
-             if(this.AircraftNext == null) {
-                  this.AircraftNext = ac;
-                  return true;
-             }
              return false;
-        }
+        }// end StartPlanebuild
     } // End Class
 } // End Namespace
