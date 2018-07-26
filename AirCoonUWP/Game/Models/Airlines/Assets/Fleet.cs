79ff866d9dc056846724eb8d52d2fb59e58c5f0b
Urbautz <40495078+Urbautz@users.noreply.github.com>
2018-07-26 10:49:50 +0200

    Added Planes, Made Serialiszable

--- AirCoonUWP/Game/Models/Airlines/Assets/Fleet.cs
+++ AirCoonUWP/Game/Models/Airlines/Assets/Fleet.cs
@@ -16,26 +16,39 @@ namespace AirCoon.Game.Models.Airlines.Assets
     {
 
         private String _Name;
-        public String Name
+        public String Name { get { return _Name; }  }
+
+        private String _Hub;
+        public Hub Hub { get { return this.Airline.Hubs[_Hub] }  }
+
+        private String _Airline;
+        public Airline Airline { get { return SaveGamePublic.SaveGame.Airlines[_Airline]; }  }
+        
+        public Dictionary<String, Plane> Planes;
+
+        public Fleet(Airline airline, Hub hub, String name = null)
         {
-            get { return _Name; }
+            if(name == null) name = airline.code + " - " + Hub.Airport.Iata;   
+            this._Name = name;
+            this_Hub = hub.Airport.Iata;
+            this._Airline = airline.Code;
+            this.Planes = new Dictionary<String, Plane>;
         }
 
-        private Airline _Airline;
-        public Airline Airline
+        public void Fleet(SerializationInfo info, StreamingContext context)
         {
-            get { return _Airline; }
-        }
-
-
-        public Fleet()
-        {
-
+            this._Name = info.GetString("Name");
+            this._Airline = info.GetString("Airline");
+            this._Hub = info.GetString("Hub");
+            this.Planes = (List<Plane>) info.GetValue("Planes", typeof(List<Plane>));
         }
 
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
-            throw new NotImplementedException();
+            info.AddValue("Name", this._Name);
+            info.AddValue("Airline", this._Airline);
+            info.AddValue("Hub", this._Hub);
+            info.AddValue("Planes", this.Planes);
         }
 
         void IAmTickable.DailyTick(Tick CurrentTick)
