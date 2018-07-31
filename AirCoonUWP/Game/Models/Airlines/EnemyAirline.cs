d1b07918331fa4145d5870711bc14b8fc1c2b8ad
Urbautz <40495078+Urbautz@users.noreply.github.com>
2018-07-31 10:48:09 +0200

    Parameters

--- AirCoonUWP/Game/Models/Airlines/EnemyAirline.cs
+++ AirCoonUWP/Game/Models/Airlines/EnemyAirline.cs
@@ -10,21 +10,42 @@ using AirCoon.Game.Models;
 
 namespace AirCoon.Game.Models.Airlines
 {
+
     public class EnemyAirline
         : Airline
     {
+    
+        private int _ActionPoints;
+        private readonly int _ActionPointsGain;
+         
+        private int _QuoteIntercontiPlanned;
+        private int _QuoteInternatPlanned;
+        private int _QuoteNationalPlanned;
+        private int _QualityLevelPlanned;
+         
+        private String [] AllowedAircraftTypes;
+
+        private EnemyAirline(String[] data) {
+            base._Code = data["Code"];
+            base._Name = data["Name"];
+            throw new NotImplementedException();
+        }
+
+        public EnemyAirline(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             throw new NotImplementedException();
         }
 
+
         public override void DailyTick(Tick CurrentTick)
         {
+            this._ActionPoints += this._ActionPointsGain;
             throw new NotImplementedException();
         }
-
-
 
         public override void MiniTick(Tick CurrentTick, int MiniTick)
         {
